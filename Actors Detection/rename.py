d7a217abf4b49fe42da4a4ec1c456e83c1ee2df0
Rami97rgb <68162300+Rami97rgb@users.noreply.github.com>
2020-10-20 17:35:11 +0200

    Update rename.py

--- Actors Detection/rename.py
+++ Actors Detection/rename.py
@@ -1,23 +1,15 @@
-# Pythono3 code to rename multiple  
-# files in a directory or folder 
-  
-# importing os module 
 import os 
   
-# Function to rename multiple files 
+#function to rename multiple files 
 def main(): 
   
     for count, filename in enumerate(os.listdir("mouna noureddine")): 
         dst ="m" + str(count) + ".jpg"
         src ='mouna noureddine/'+ filename 
         dst ='mouna noureddine/'+ dst 
-          
-        # rename() function will 
-        # rename all the files 
+
         os.rename(src, dst) 
   
-# Driver Code 
 if __name__ == '__main__': 
-      
-    # Calling main() function 
+  
     main() 
